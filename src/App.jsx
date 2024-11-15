dbf242f18c6e57fec663712a9e3e12e7bf7104d2
eugen <aleksandrov.eugen@gmail.com>
2024-11-15 19:02:41 +0200

    Move last component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCard from './components/HomeCard';
 import JobListings from './components/JobListings';
+import ViewAllJobs from './ViewAllJobs';
 
 const App = () => {
   return (
@@ -10,14 +11,7 @@ const App = () => {
       <Hero />
       <HomeCard />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-          >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
