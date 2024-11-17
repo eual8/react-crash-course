25f69150b5059053a99d9232f690701ea0dc7fa8
eugen <aleksandrov.eugen@gmail.com>
2024-11-17 17:28:58 +0200

    Update links

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import {
-  Route, 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  RouterProvider
-} from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import HomePage from './pages/HomePage';
-import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFoundPage';
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={ <MainLayout /> }>
-      <Route index element={ <HomePage /> }></Route>
-      <Route path='jobs' element={ <JobsPage /> }></Route>
-      <Route path='*' element={ <NotFoundPage /> }></Route>
-    </Route>)
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />}></Route>
+      <Route path="jobs" element={<JobsPage />}></Route>
+      <Route path="*" element={<NotFoundPage />}></Route>
+    </Route>
+  )
 );
 
-
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
