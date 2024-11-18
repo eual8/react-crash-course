2c97581c443e8288e62a8b202ec54dbc562920d5
eugen <aleksandrov.eugen@gmail.com>
2024-11-18 17:54:34 +0200

    time 2h 23m

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />}></Route>
       <Route path="/jobs" element={<JobsPage />}></Route>
+      <Route path="/add-job" met element={<AddJobPage />}></Route>
       <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}></Route>
       <Route path="*" element={<NotFoundPage />}></Route>
     </Route>
